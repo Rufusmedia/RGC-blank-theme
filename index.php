ea05829a8845ceec1f4175d6ab2e8c610920a258
Jeromy Condon <jeromy@rustygeorge.com>
2016-05-16 11:55:12 -0700

    Add normalize, base sass structure, and build out basic grid system

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<!doctype html>
+<html lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <link rel="shortcut icon" href="<?php bloginfo('template_directory') ?>/assets/favicon.png">
+        <link rel="stylesheet" href="<?php bloginfo('template_directory') ?>/style.css">
+    </head>
+    <body>
+        <!--[if lte IE 9]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+
+    </body>
+</html>
