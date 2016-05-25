18514e40abcefe9855d967d7fb7224451eff6523
Jeromy Condon <jeromy@rustygeorge.com>
2016-05-25 16:46:34 -0700

    Style browse happy message.

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
     </head>
     <body>
         <!--[if lte IE 9]>
-            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/?locale=en">upgrade your browser</a> to improve your experience.</p>
+            <div class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/?locale=en" target="_blank">upgrade your browser</a> to improve your experience.</div>
         <![endif]-->
 
         <header>
