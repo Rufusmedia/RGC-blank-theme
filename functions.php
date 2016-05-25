df659919dc873f7bdbf839956f73d8b049318a5c
Jeromy Condon <jeromy@rustygeorge.com>
2016-05-25 16:34:41 -0700

    change function names to RGC

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@
 |====================================================
 */
 // Disable support for comments and trackbacks in post types
-function df_disable_comments_post_types_support() {
+function rgc_disable_comments_post_types_support() {
     $post_types = get_post_types();
     foreach ($post_types as $post_type) {
         if(post_type_supports($post_type, 'comments')) {
@@ -20,7 +20,7 @@ function df_disable_comments_post_types_support() {
         }
     }
 }
-add_action('admin_init', 'df_disable_comments_post_types_support');
+add_action('admin_init', 'rgc_disable_comments_post_types_support');
 
 // remove comments link from admin bar
 function my_admin_bar_render() {
@@ -30,39 +30,39 @@ function my_admin_bar_render() {
 add_action( 'wp_before_admin_bar_render', 'my_admin_bar_render' );
 
 // Close comments on the front-end
-function df_disable_comments_status() {
+function rgc_disable_comments_status() {
     return false;
 }
-add_filter('comments_open', 'df_disable_comments_status', 20, 2);
-add_filter('pings_open', 'df_disable_comments_status', 20, 2);
+add_filter('comments_open', 'rgc_disable_comments_status', 20, 2);
+add_filter('pings_open', 'rgc_disable_comments_status', 20, 2);
 
 // Hide existing comments
-function df_disable_comments_hide_existing_comments($comments) {
+function rgc_disable_comments_hide_existing_comments($comments) {
     $comments = array();
     return $comments;
 }
-add_filter('comments_array', 'df_disable_comments_hide_existing_comments', 10, 2);
+add_filter('comments_array', 'rgc_disable_comments_hide_existing_comments', 10, 2);
 
 // Remove comments page in menu
-function df_disable_comments_admin_menu() {
+function rgc_disable_comments_admin_menu() {
     remove_menu_page('edit-comments.php');
 }
-add_action('admin_menu', 'df_disable_comments_admin_menu');
+add_action('admin_menu', 'rgc_disable_comments_admin_menu');
 
 // Redirect any user trying to access comments page
-function df_disable_comments_admin_menu_redirect() {
+function rgc_disable_comments_admin_menu_redirect() {
     global $pagenow;
     if ($pagenow === 'edit-comments.php') {
         wp_redirect(admin_url()); exit;
     }
 }
-add_action('admin_init', 'df_disable_comments_admin_menu_redirect');
+add_action('admin_init', 'rgc_disable_comments_admin_menu_redirect');
 
 // Remove comments metabox from dashboard
-function df_disable_comments_dashboard() {
+function rgc_disable_comments_dashboard() {
     remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
 }
-add_action('admin_init', 'df_disable_comments_dashboard');
+add_action('admin_init', 'rgc_disable_comments_dashboard');
 
 /*
 | ===================================================
